b88b72b91f0693d0c0ac1adde4c9caf22d1555d4
Lee Hicks <leehicks@dreamfactory.com>
2015-05-15 16:26:50 -0400

    clearing the way for one time migrations and seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,18 +3,61 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class DatabaseSeeder extends Seeder {
+class DatabaseSeeder extends Seeder
+{
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Model::unguard();
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
 
-		// $this->call('UserTableSeeder');
-	}
-
+        if ( class_exists( 'DreamFactory\\Rave\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\SqlDb\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\SqlDb\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\MongoDb\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\MongoDb\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\CouchDb\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\CouchDb\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\Rws\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\Rws\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\Aws\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\Aws\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\Azure\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\Azure\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\Rackspace\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\Rackspace\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\Rave\\Salesforce\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\Rave\\Salesforce\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\DSP\\OAuth\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\DSP\\OAuth\\Database\\Seeds\\DatabaseSeeder' );
+        }
+        if ( class_exists( 'DreamFactory\\DSP\\ADLdap\\Database\\Seeds\\DatabaseSeeder' ) )
+        {
+            $this->call( 'DreamFactory\\DSP\\ADLdap\\Database\\Seeds\\DatabaseSeeder' );
+        }
+    }
 }
