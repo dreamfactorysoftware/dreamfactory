55cf391d0660d8128a2e33a44f42ffdac5711c2b
Lee Hicks <leehicks@dreamfactory.com>
2015-06-24 20:46:50 -0400

    df.php changes

--- config/df.php
+++ config/df.php
@@ -25,6 +25,7 @@ return [
     /** Enable/disable detailed CORS logging */
     'log_cors_info'                => false,
     'default_cache_ttl'            => env('CACHE_TTL', 300),
+    'allow_forever_sessions'       => false,
     'cors'                         => [
         'defaults' => [
             'supportsCredentials' => false,
