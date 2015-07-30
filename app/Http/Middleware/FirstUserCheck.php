aac5b401e62a282ae2f5a72e00ceea19c2023933
Arif Islam <arif@dreamfactory.com>
2015-07-30 12:42:40 -0400

    removing cache dependency from db migration and seeder... partially

--- app/Http/Middleware/FirstUserCheck.php
+++ app/Http/Middleware/FirstUserCheck.php
@@ -4,6 +4,7 @@ namespace DreamFactory\Http\Middleware;
 
 use DreamFactory\Core\Utility\CacheUtilities;
 use Illuminate\Contracts\Routing\Middleware;
+use Illuminate\Database\QueryException;
 use Closure;
 
 class FirstUserCheck implements Middleware
@@ -12,23 +13,32 @@ class FirstUserCheck implements Middleware
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $route = $request->getPathInfo();
 
-        if('/setup' !== $route) {
-            if(!CacheUtilities::hasServiceTable()){
-                \Artisan::call('migrate');
-                \Artisan::call('db:seed');
-                CacheUtilities::resetServiceTableExists();
-            }
+        if ('/setup' !== $route) {
+            try {
+                if (!CacheUtilities::adminExists()) {
+                    return redirect()->to('/setup');
+                }
+            } catch (QueryException $e) {
+                $code = $e->getCode();
 
-            if(!CacheUtilities::adminExists()) {
-                return redirect()->to('/setup');
+                if($code === '42S02') {
+                    //Mysql base table or view not found.
+                    \Artisan::call('migrate');
+                    \Artisan::call('db:seed');
+
+                    return redirect()->to('/setup');
+                } else {
+                    throw $e;
+                }
             }
         }
 
