ab8f1ae40bbea2dba1cad4e57b9a922877c8493c
David Weiner <davidweiner@dreamfactory.com>
2015-08-01 14:43:16 -0400

    Updated cache path to use gethostname() if $_SERVER['SERVER_NAME'] is not set to

--- config/cache.php
+++ config/cache.php
@@ -44,7 +44,8 @@ return [
 
 		'file' => [
 			'driver' => 'file',
-			'path'   => storage_path().'/framework/cache/'.md5($_SERVER['SERVER_NAME']),
+			'path'   => storage_path().'/framework/cache/'.
+			\Illuminate\Support\Arr::has($_SERVER, 'SERVER_NAME')? md5($_SERVER['SERVER_NAME']) : md5(gethostname()),
 		],
 
 		'memcached' => [
