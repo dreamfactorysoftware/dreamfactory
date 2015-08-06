8c0d800d9f2be59872d950b416629357f3c68a1e
David Weiner <davidweiner@dreamfactory.com>
2015-08-06 12:03:21 -0400

    Can't have an empty array for a db connection

--- config/database.php
+++ config/database.php
@@ -84,7 +84,17 @@ return [
 			'prefix'   => '',
 		],
 
-		'dreamfactory' => [],
+		'dreamfactory' => [
+			'driver'    => 'mysql',
+			'host'      => env('DB_HOST', 'localhost'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+			'strict'    => false,
+		],
 
 	],
 
