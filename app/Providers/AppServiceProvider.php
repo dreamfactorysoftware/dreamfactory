d3c714e8082a24674e228637aa680dc87fafc8b8
Charles Harmon <charles@harmonmediagroup.com>
2017-01-13 09:36:06 -0500

    Added separate storage cache for limits.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        if ($this->app->environment() !== 'production') {
+            $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
+        }
         $this->app->bind('Illuminate\Contracts\Auth\Registrar', Registrar::class);
     }
 }
