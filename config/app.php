b50dcaf81cb0526607aed1419bc1336ea6a8251d
Lee Hicks <leehicks@dreamfactory.com>
2017-06-06 14:41:46 -0400

    composer and docs updated

--- config/app.php
+++ config/app.php
@@ -112,7 +112,7 @@ return [
     | This is the version of your application, not the version of the API.
     */
 
-    'version' => '2.6.0',
+    'version' => '2.7.0',
 
     /*
     |--------------------------------------------------------------------------
