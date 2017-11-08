4f48efe6d950bab17acfe8a9426f11cec7d779b5
vagrant <vagrant@dreamfactory>
2017-11-08 20:08:49 +0000

    Add back first user middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,7 @@ class Kernel extends HttpKernel
 //            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
 //            \DreamFactory\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \DreamFactory\Http\Middleware\FirstUserCheck::class,
         ],
 
 //        'api' => [
