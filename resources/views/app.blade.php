927f7ed80614983a577f33fe2596b4f5ddbebfcc
VolodymyrTymtsiv <volodymyrt@interlink-ua.com>
2018-08-02 17:44:32 +0300

    Replace js files from sdn to local.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,8 +14,8 @@
 	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<script src="/js/html5shiv.min.js"></script>
+		<script src="/js/respond.min.js"></script>
 	<![endif]-->
     <style>
         body {
@@ -61,7 +61,7 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	<script src="/js/jquery/2.1.3/jquery.min.js"></script>
+	<script src="/js/bootstrap/3.3.1/bootstrap.min.js"></script>
 </body>
 </html>
--- resources/views/firstUser.blade.php
+++ resources/views/firstUser.blade.php
@@ -17,7 +17,7 @@
     <!-- DSP UI Styles & Code -->
     <link rel="stylesheet" href="/css/df.main.css">
 
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src="/js/jquery/2.1.1/jquery.min.js"></script>
 </head>
 <body>
 
@@ -195,6 +195,6 @@
 
 
 <!-- Scripts -->
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="/js/bootstrap/3.2.0/bootstrap.min.js"></script>
 </body>
 </html>
