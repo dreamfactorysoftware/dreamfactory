9bac3b7ea635b806cdafbe1f386c917eb222af69
OlegL <olegl@softwareplanetgroup.com>
2019-02-14 17:00:36 +0200

    Add LogsDB Configure custom HTTP Logger for writing access log into Mongo

---
+++ utility/http-logger/MongoLogWriter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Utility\HttpLogger;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class MongoLogWriter implements \Spatie\HttpLogger\LogWriter
+{
+    public function logRequest(Request $request)
+    {
+        $method = strtoupper($request->getMethod());
+
+        $uri = $request->getPathInfo();
+
+        $bodyAsJson = json_encode($request->except(config('http-logger.except')));
+
+        $files = array_map(function (UploadedFile $file) {
+            return $file->path();
+        }, iterator_to_array($request->files));
+
+        $timestamp = \Carbon\Carbon::now();
+
+        $message = "{$method} {$uri} - Body: {$bodyAsJson} - Files: ".implode(', ', $files);
+
+        \DB::connection('logsdb')->collection('access')->insert(['timestamp' => $timestamp->toDateTimeString(), 'method' => $method, 'uri' => $uri, 'body' => $bodyAsJson]);
+
+        Log::info($message);
+    }
+}
