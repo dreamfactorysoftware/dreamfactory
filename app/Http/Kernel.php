f9044267174c6bab466d779e97dd800a3ecf0b9c
taras <tarasvii@interlink-ua.com>
2019-09-18 20:38:18 +0300

    Fix wrong class namespace

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \DreamFactory\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
     ];
 
