c48712ac57a00b67813c8c56467b57044acea767
Laravel Shift <shift@laravelshift.com>
2019-09-24 12:16:09 +0000

    Adopt Laravel coding style
    The Laravel framework adopts the PSR-2 coding style with some additions.
    Laravel apps *should* adopt this coding style as well.
    
    However, Shift allows you to customize the adopted coding style through
    by using your [PHP CS Fixer][1] config within your project.
    
    You may use [Shift's .php_cs][2] file as a base.
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,11 +2,11 @@
 
 namespace DreamFactory\Http\Controllers\Auth;
 
-use DreamFactory\User;
 use DreamFactory\Http\Controllers\Controller;
+use DreamFactory\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
