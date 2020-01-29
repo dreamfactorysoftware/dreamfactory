b893af6d971170cc6eb094f6a5863a886aab80c8
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2020-01-29 12:44:02 +0200

    DP-160 Update platform favicon
    - Update favicon to 'lamp'
    
    - Bump app version

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | This is the version of your application, not the version of the API.
     */
 
-    'version' => '4.0.0',
+    'version' => '4.0.1',
 
     /*
     |--------------------------------------------------------------------------
