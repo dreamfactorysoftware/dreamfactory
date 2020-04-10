5bfe29683864dfa023d644cae7db55d4f945702d
Yaroslav Mokhurenko <yaroslavmo@softwareplanetgroup.com>
2020-04-10 18:13:15 +0300

    DP-190 - Fixed DreamFactory OpenID connector
    DP-183 - Added support LDAP for PHP 7.4+
    
    DP-189 - Added the EXTERNAL_IP variable
    
    DP-180 - Fixed Scheduler UI
    
    DFSAAS-119 - Added phone number to df:setup command
    
    DP-182 Investigate strange 4.0.1 perfomance issue on CentOS
     - Fix PHP 7.4 compatibility issue

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | This is the version of your application, not the version of the API.
     */
 
-    'version' => '4.1.0',
+    'version' => '4.1.1',
 
     /*
     |--------------------------------------------------------------------------
