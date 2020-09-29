e5d64c1c275c67e487d16e2a7f92e55a416cf734
Jason Gilmore <wj@wjgilmore.com>
2020-09-29 16:04:44 -0400

    Bumped version number

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | This is the version of your application, not the version of the API.
     */
 
-    'version' => '4.3.1',
+    'version' => '4.3.2',
 
     /*
     |--------------------------------------------------------------------------
