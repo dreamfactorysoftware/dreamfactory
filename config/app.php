f4ba700eb8ce6dc6216969bbde2fc5ec04712ed3
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2021-01-22 13:52:08 +0200

    Release 4.4.0

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | This is the version of your application, not the version of the API.
     */
 
-    'version' => '4.3.3',
+    'version' => '4.4.0',
 
     /*
     |--------------------------------------------------------------------------
