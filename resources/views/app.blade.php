32f55d8d50bf7c218124502f451d492ffd963719
Yaroslav Mokhurenko <yaroslavmo@softwareplanet.uk.com>
2021-05-14 14:52:00 +0300

    DP-320 Resolve dreamfactorysoftware/dreamfactory dependabot security alerts
    - Bump jquery
    - Build javascript

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -61,7 +61,7 @@
 	@yield('content')
 
 	<!-- Scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
 </body>
 </html>
