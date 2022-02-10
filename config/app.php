c6ccd8a4225c3121c1f5192726a3b3131cf88bdf
yaroslavmo <yaroslavmo@softwareplanet.uk.com>
2022-02-10 10:47:27 +0200

    Bump version

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | This is the version of your application, not the version of the API.
     */
 
-    'version' => '4.10.1',
+    'version' => '4.10.2',
 
     /*
     |--------------------------------------------------------------------------
