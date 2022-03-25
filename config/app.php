6bf610f7a77f36bc51a721783efb9ba43e8d6637
Tomo Norman <tomo.l.norman@gmail.com>
2022-03-25 10:28:08 +0900

    Bump Version

--- config/app.php
+++ config/app.php
@@ -10,7 +10,7 @@ return [
     | This is the version of your application, not the version of the API.
     */
 
-    'version' => '4.10.2',
+    'version' => '4.11.0',
 
     /*
     |--------------------------------------------------------------------------
