10856ca545037e6b00d13efa38f3da4a8f856534
Tomo Norman <tomo.l.norman@gmail.com>
2022-05-26 15:38:56 +0900

    DP-512 Add Api Limit to login api endpoints (template). Add Debian 11 support

--- installers/source/fedora.sh
+++ installers/source/fedora.sh
@@ -137,6 +137,8 @@ install_nginx () {
 
   # Create nginx site entry
   echo "
+#Default API call rate -> Here is set to 1 per second, and is later defined in the location /api/v2 section
+limit_req_zone \$binary_remote_addr zone=mylimit:10m rate=1r/s;
 server {
 
   listen 80 default_server;
@@ -177,6 +179,18 @@ server {
   }
   location ~ /web.config {
     deny all;
+  }
+  #By default we will limit login calls here using the limit_req_zone set above. The below will allow 1 per second over
+  # 5 seconds (so 5 in 5 seconds)from a single IP  before returning a 429 too many requests. Adjust as needed.
+  location /api/v2/user/session {
+    try_files \$uri \$uri/ /index.php?\$args;
+    limit_req zone=mylimit burst=5 nodelay;
+    limit_req_status 429;
+  }
+  location /api/v2/system/admin/session {
+    try_files \$uri \$uri/ /index.php?\$args;
+    limit_req zone=mylimit burst=5 nodelay;
+    limit_req_status 429;
   }
 }" >/etc/nginx/conf.d/dreamfactory.conf
 
