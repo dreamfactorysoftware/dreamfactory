424655866082c34d769f7af835a2e1077c002d7b
Danil Lytvyn <daniilly@softwareplanetgroup.com>
2023-02-23 18:15:43 +0200

    DP-460 Upgrade to PHP 8.1 / Laravel 9
    Update package dependencies
    
    Upgrade to Laravel 9
    
    The installers update includes several changes:
    - Support of Fedora v34, v35 and Ubuntu v18 has been discontinued.
    - Add support for Fedora v37 and Ubuntu v22.
    - Upgrade dependencies to PHP 8.1.
    
    There is an issue with the include in the latest version
    of unixodbc-dev v2.3.11. As a temporary solution, we have
    reverted to a previous version that does not have this problem.
    For more information, please refer to the related issues:
    - microsoft/linux-package-repositories#36
    - microsoft/linux-package-repositories#39

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace DreamFactory\Exceptions;
 
-use Exception;
+use Throwable;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -34,7 +34,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return void
      */
-    public function report(Exception $exception)
+    public function report(Throwable $exception)
     {
         parent::report($exception);
     }
@@ -46,7 +46,7 @@ class Handler extends ExceptionHandler
      * @param  \Exception  $exception
      * @return \Illuminate\Http\Response
      */
-    public function render($request, Exception $exception)
+    public function render($request, Throwable $exception)
     {
         return parent::render($request, $exception);
     }
