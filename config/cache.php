69516809f06e21511c2f0be9934b799583d3cd66
Shift <shift@laravelshift.com>
2024-07-15 20:02:46 +0000

    Streamline config files

--- config/cache.php
+++ config/cache.php
@@ -4,48 +4,7 @@ use Illuminate\Support\Str;
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Cache Store
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default cache connection that gets used while
-    | using this caching library. This connection is used when another is
-    | not explicitly specified when executing a given caching function.
-    |
-    | Supported: "apc", "array", "database", "file", "memcached", "redis"
-    |
-    */
-
-    'default' => env('CACHE_DRIVER', 'file'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Stores
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the cache "stores" for your application as
-    | well as their drivers. You may even define multiple stores for the
-    | same cache driver to group types of items stored in your caches.
-    |
-    */
-
     'stores' => [
-
-        'apc' => [
-            'driver' => 'apc',
-        ],
-
-        'array' => [
-            'driver' => 'array',
-        ],
-
-        'database' => [
-            'driver' => 'database',
-            'table' => env('CACHE_TABLE', 'cache'),
-            'connection' => env('DB_CONNECTION', 'sqlite'),
-        ],
-
         'file' => [
             'driver' => 'file',
             'path' => env('CACHE_PATH', env('DF_CACHE_PATH', storage_path('framework/cache/data'))),
@@ -69,35 +28,7 @@ return [
                 ],
             ],
         ],
-
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'cache',
-        ],
-
     ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Key Prefix
-    |--------------------------------------------------------------------------
-    |
-    | When utilizing a RAM based store such as APC or Memcached, there might
-    | be other applications utilizing the same cache. So, we'll specify a
-    | value to get prefixed to all our keys so we can avoid collisions.
-    |
-    */
-
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Default Time To Live
-    |--------------------------------------------------------------------------
-    |
-    | When the application does not specify a TTL, use this instead.
-    |
-    */
 
     'default_ttl' => env('CACHE_DEFAULT_TTL', env('DF_CACHE_TTL', 18000)), // old env for upgrades
 
--- config/queue.php
+++ config/queue.php
@@ -2,38 +2,7 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Queue Driver
-    |--------------------------------------------------------------------------
-    |
-    | Laravel's queue API supports an assortment of back-ends via a single
-    | API, giving you convenient access to each back-end using the same
-    | syntax for each one. Here you may set the default queue driver.
-    |
-    | Supported: "sync", "database", "beanstalkd", "sqs", "redis", "null"
-    |
-    */
-
-    'default' => env('QUEUE_CONNECTION', 'sync'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Queue Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the connection information for each server that
-    | is used by your application. A default configuration has been added
-    | for each back-end shipped with Laravel. You are free to add more.
-    |
-    */
-
     'connections' => [
-
-        'sync' => [
-            'driver' => 'sync',
-        ],
-
         'database' => [
             'driver' => 'database',
             'database' => env('DB_CONNECTION', 'sqlite'),
@@ -41,46 +10,6 @@ return [
             'queue' => env('QUEUE_NAME', 'default'),
             'retry_after' => env('QUEUE_RETRY_AFTER', 90),
         ],
-
-        'beanstalkd' => [
-            'driver' => 'beanstalkd',
-            'host' => env('QUEUE_HOST', 'localhost'),
-            'queue' => env('QUEUE_NAME', 'default'),
-            'retry_after' => env('QUEUE_RETRY_AFTER', 90),
-        ],
-
-        'sqs' => [
-            'driver' => 'sqs',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-            'prefix' => env('SQS_PREFIX', 'https://sqs.us-east-1.amazonaws.com/your-account-id'),
-            'queue' => env('QUEUE_NAME', 'default'),
-        ],
-
-        'redis' => [
-            'driver' => 'redis',
-            'connection' => 'queue',
-            'queue' => env('QUEUE_NAME', 'default'),
-            'retry_after' => env('QUEUE_RETRY_AFTER', 90),
-        ],
-
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Failed Queue Jobs
-    |--------------------------------------------------------------------------
-    |
-    | These options configure the behavior of failed queue job logging so you
-    | can control which database and table are used to store the jobs that
-    | have failed. You may change them to any database / table you wish.
-    |
-    */
-
-    'failed' => [
-        'database' => env('DB_CONNECTION', 'sqlite'),
-        'table' => 'failed_jobs',
     ],
 
 ];
