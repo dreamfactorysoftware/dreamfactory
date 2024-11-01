ea3a9b362dbb88cede6028b49b88c269f1e91882
Kevin McGahey <kevinmcgahey1114@gmail.com>
2024-11-01 16:29:45 -0700

    csfr and remove graphql

--- app/Http/Controllers/SplashController.php
+++ app/Http/Controllers/SplashController.php
@@ -70,9 +70,6 @@ class SplashController extends Controller
             $request = \Request::instance();
             $method = $request->method();
 
-            \Log::debug('CSRF Token received: ' . $request->header('X-CSRF-TOKEN'));
-            \Log::debug('Session Token: ' . csrf_token());
-
             if ($method === Verbs::GET) {
                 return view('setup', [
                     'version' => config('app.version'),
